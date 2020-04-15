213efaae03f468b5fe813be1c3d1452a36123163
ashokgithub15feb <ashokcse126@gmail.com>
2020-04-16 00:10:53 +0530

    Second Commit

--- src/main/java/com/stacksimplify/restservices/SpringbootBuildingblocksApplication.java
+++ src/main/java/com/stacksimplify/restservices/SpringbootBuildingblocksApplication.java
@@ -2,6 +2,7 @@ package com.stacksimplify.restservices;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+//First Spring Boot Application and push the code in GIT Repository through STS tools
 
 @SpringBootApplication
 public class SpringbootBuildingblocksApplication {
