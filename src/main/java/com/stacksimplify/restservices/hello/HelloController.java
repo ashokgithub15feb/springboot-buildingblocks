ecedef8bc3023816eae81524ed6f14754adc010f
ashokgithub15feb <ashokcse126@gmail.com>
2020-04-16 23:23:21 +0530

    Hello World Base

---
+++ src/main/java/com/stacksimplify/restservices/hello/HelloController.java
@@ -0,0 +1,28 @@
+package com.stacksimplify.restservices.hello;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+//Controller
+@RestController
+public class HelloController {
+
+	//Simple method
+	//URI - helloworld
+	//GET
+	
+	//@RequestMapping(method = RequestMethod.GET, path = "helloworld")
+	@GetMapping("/helloworld1")
+	public String helloWorld()
+	{
+		return "Hello World";
+	}
+	
+	@GetMapping("/helloworldbean")
+	public UserDetails helloWorldBean()
+	{
+		return new UserDetails("Ajit", "H", "Bangalore");
+	}
+}
