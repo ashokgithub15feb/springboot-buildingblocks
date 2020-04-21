cf877026a3b4ee80c136c29ba550457552d5350c
ashokgithub15feb <ashokcse126@gmail.com>
2020-04-21 22:53:58 +0530

    Swagger - First Commit

---
+++ src/main/java/com/stacksimplify/restservices/config/SwaggerConfig.java
@@ -0,0 +1,44 @@
+//
+//package com.stacksimplify.restservices.config;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//import springfox.documentation.builders.ApiInfoBuilder;
+//import springfox.documentation.builders.PathSelectors;
+//import springfox.documentation.builders.RequestHandlerSelectors;
+//import springfox.documentation.service.ApiInfo;
+//import springfox.documentation.service.Contact;
+//import springfox.documentation.spi.DocumentationType;
+//import springfox.documentation.spring.web.plugins.Docket;
+//import springfox.documentation.swagger2.annotations.EnableSwagger2;
+//
+//@Configuration
+//
+//@EnableSwagger2
+//public class SwaggerConfig {
+//
+//	@Bean
+//	public Docket api() {
+//		return new Docket(DocumentationType.SWAGGER_2)
+//				.apiInfo(getApiInfo())
+//				.select()
+//				.apis(RequestHandlerSelectors.basePackage("com.stacksimplify.restservices"))
+//				.paths(PathSelectors.ant("/users/**")).build();
+//	}
+//
+//	// Swagger Metadata: http:localhost:8080/v2/api-doc
+//	// Swagger UI URL: http://localhost:8080/swagger-ui.html
+//
+//	private ApiInfo getApiInfo()
+//  {
+//  		return new ApiInfoBuilder()
+//  					.title("StackSimplify user Management Service")
+//  					.description("This pages lists all api's of User Management")
+//                   .version("2.0")
+//                   .contact(new Contact("Ashok", "https://stacksimplifiy.com", "ashok@gmail.com"))
+//                   .license("License 2.0")
+//                   .licenseUrl("https://stacksimplifiy.com/license.html").build();
+//  }
+//
+//}
